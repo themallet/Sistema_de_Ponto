134d1c61a4e2712aff41231768c5ef0f4ae1db07
themallet <amandamaiaballet@gmail.com>
2025-03-14 12:03:27 -0300

    Fazendo Pag login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/simbolo_newbyte.png';
+import bg from './assets/background.png';
 
 function App() {
   return (
-    <div className="container">
-      <div className="logo-container">
-        <img src={logo} alt="Logo Newbyte" className="logo" />
-        <h1>Ponto Newbyte</h1>
+    <div className="login-container">
+      {/* Lado esquerdo com imagem */}
+      <div
+        className="login-left"
+        style={{ backgroundImage: `url(${bg})` }}
+      ></div>
+
+      {/* Lado direito com o formulário */}
+      <div className="login-right">
+        <div className="login-box">
+          <img src={logo} alt="Logo NewByte" className="logo" />
+          <h2>Bem Vindo!</h2>
+
+          <form>
+            <input type="text" placeholder="ID Empresarial" />
+            <div className="password-field">
+              <input type="password" placeholder="Senha" />
+              <span className="eye-icon">👁️</span> {/* Pode trocar depois */}
+            </div>
+
+            <button type="submit">Entrar</button>
+          </form>
+
+          <div className="help-text">
+            Problemas no Login? <a href="#">Fale Conosco</a>
+          </div>
+
+          <footer>© NewByte - 2025</footer>
+        </div>
       </div>
     </div>
   );
